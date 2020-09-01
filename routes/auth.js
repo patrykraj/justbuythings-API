cb18382a3288f875dd1ae6f0ca63215669d85648
Patryk Krajewski <patrykraj@interia.pl>
2020-09-01 14:58:07 +0200

    Implemented password hashing with bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,33 +1,46 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const bcrypt = require("bcryptjs");
 
-//Validation
-const Joi = require("@hapi/joi");
-
-const schema = Joi.object({
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).email(),
-  password: Joi.string().min(6).required(),
-});
+const {
+  registerValidation,
+  loginValidation,
+} = require("../validators/validation");
 
 router.post("/register", async (req, res) => {
-  const { error } = schema.validate(req.body);
+  //validation before creating user
+  const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const { name, email, password } = req.body;
 
+  //check if the email is unique
+  const emailExists = await User.findOne({ email: email });
+  if (emailExists) return res.status(400).send("Email already exists.");
+
+  //hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
   const user = new User({
     name,
     email,
-    password,
+    password: hashedPassword,
   });
 
   try {
     const savedUser = await user.save();
-    res.send(savedUser);
+    res.send({ user: user._id });
   } catch (error) {
     res.status(400).send(error);
   }
+});
+
+router.post("/login", async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const { name, email, password } = req.body;
 });
 
 module.exports = router;
