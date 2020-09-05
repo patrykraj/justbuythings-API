a20f8a6351cd372752d7d52a2fa603e174e4f329
Patryk Krajewski <patrykraj@interia.pl>
2020-09-06 01:35:45 +0200

    Implemented cancel order route

--- routes/orders.js
+++ routes/orders.js
@@ -1,18 +1,39 @@
 const router = require("express").Router();
 const User = require("../model/User");
 
-router.get("/", async (req, res) => {
-  let user = await User.findOne({ email: req.email });
-  transactions = user.transactions;
-  res.send(transactions);
-});
-
-router.post("/buy", async (req, res) => {
+router.patch("/buy", async (req, res) => {
   let update = await User.updateOne(
     { email: req.body.email },
     { $push: { transactions: req.body.products } }
   );
   let user = await User.findOne({ email: req.body.email });
+
+  user = {
+    name: user.name,
+    lastname: user.lastname,
+    email: user.email,
+    transactions: user.transactions,
+  };
+  res.send(user);
+});
+
+router.patch("/cancel", async (req, res) => {
+  let update = await User.updateOne(
+    { "transactions.renderId": req.body.id },
+    {
+      $set: {
+        "transactions.$.cancelled": true,
+      },
+    }
+  );
+  let user = await User.findOne({ email: req.body.email });
+
+  user = {
+    name: user.name,
+    lastname: user.lastname,
+    email: user.email,
+    transactions: user.transactions,
+  };
   res.send(user);
 });
 
