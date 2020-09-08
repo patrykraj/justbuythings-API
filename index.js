39e8b7638600c3264cdd5595a2c9e8fd0af8b5e5
Patryk Krajewski <patrykraj@interia.pl>
2020-09-08 14:18:21 +0200

    Changed start script and added welcome route

--- index.js
+++ index.js
@@ -41,14 +41,9 @@ app.use(bodyParser.json());
 app.use("/api/user", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/orders", ordersRoute);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.use("/", (req, res) => {
+  res.send("<h1>Welcome</h1>");
+});
 
 const PORT = process.env.PORT || 5000;
 
