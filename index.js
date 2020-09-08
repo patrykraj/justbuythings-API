a5bf839a933e4039f006f928c316ade139dbe9c2
Patryk Krajewski <patrykraj@interia.pl>
2020-09-09 01:41:41 +0200

    Public update, removed console logs

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-//Import Routes
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 const ordersRoute = require("./routes/orders");
 
 dotenv.config();
 
-//Connect to DB
 mongoose
   .connect(process.env.DB_CONNECT, {
     useNewUrlParser: true,
@@ -31,7 +29,6 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-//Middleware
 app.use(express.json());
 app.use(
   bodyParser.urlencoded({
@@ -40,17 +37,13 @@ app.use(
 );
 app.use(bodyParser.json());
 
-//Route Middlewares
 app.use("/api/user", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/orders", ordersRoute);
 
-//Handle production
 if (process.env.NODE_ENV === "production") {
-  // Static folder
   app.use(express.static(__dirname + "/public"));
 
-  //Handle SPA
   app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
 }
 
--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,6 @@ const {
 } = require("../validators/validation");
 
 router.post("/register", async (req, res) => {
-  //validation before creating user
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -20,11 +19,9 @@ router.post("/register", async (req, res) => {
   if (password !== repeatPassword)
     return res.status(400).send("Passwords must be the same");
 
-  //check if the email is unique
   const emailExists = await User.findOne({ email: email });
   if (emailExists) return res.status(400).send("Email already exists.");
 
-  //hash password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -54,24 +51,19 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-  console.log(req.body, "req");
   const { error } = loginValidation(req.body);
   if (error) {
-    console.log(error.details[0].message, "error!!!!!!!!");
     return res.status(400).send(error.details[0].message);
   }
 
   const { email, password } = req.body;
 
-  //check if the email exists
   const user = await User.findOne({ email: email });
   if (!user) return res.status(400).send("Email does not exist.");
 
-  //check if password is correct
   const validPass = await bcrypt.compare(password, user.password);
   if (!validPass) return res.status(400).send("Invalid password.");
 
-  //Create and assing a token
   const token = jwt.sign(
     {
       name: user.name,
