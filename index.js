7121cd46806db4b896b41d4863746caa24c230b9
Patryk Krajewski <patrykraj@interia.pl>
2020-09-08 18:28:42 +0200

    Added static folder and prep spa

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+//Import Routes
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 const ordersRoute = require("./routes/orders");
 
 dotenv.config();
 
+//Connect to DB
 mongoose
   .connect(process.env.DB_CONNECT, {
     useNewUrlParser: true,
@@ -23,11 +26,12 @@ mongoose
   .catch((err) => console.log("Could not connect to mongoDB", err));
 
 var corsOptions = {
-  origin: "*",
+  origin: "http://localhost:8080",
 };
 
 app.use(cors(corsOptions));
 
+//Middleware
 app.use(express.json());
 app.use(
   bodyParser.urlencoded({
@@ -36,12 +40,19 @@ app.use(
 );
 app.use(bodyParser.json());
 
+//Route Middlewares
 app.use("/api/user", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/orders", ordersRoute);
-app.use("/", (req, res) => {
-  res.send("<h1>Welcome</h1>");
-});
+
+//Handle production
+if (process.env.NODE_ENV === "production") {
+  // Static folder
+  app.use(express.static(__dirname + "/public"));
+
+  //Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
 
 const PORT = process.env.PORT || 5000;
 
